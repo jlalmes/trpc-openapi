9d0dd75329645ea3a34d7cdbe3d49c9d0dcc743d
James Berry <jb@jamesbe.com>
2022-08-22 21:13:25 +0100

    feat/unlink-proc-methods

--- src/generator/paths.ts
+++ src/generator/paths.ts
@@ -3,105 +3,25 @@ import { OpenAPIV3 } from 'openapi-types';
 
 import { OpenApiRouter } from '../types';
 import { getPathParameters, normalizePath } from '../utils/path';
-import { forEachOpenApiProcedure, getInputOutputParsers } from '../utils/procedure';
+import {
+  forEachOpenApiProcedure,
+  getInputOutputParsers,
+  mergeProcedureRecords,
+} from '../utils/procedure';
 import { getParameterObjects, getRequestBodyObject, getResponsesObject } from './schema';
+
+const acceptsRequestBody = (httpMethod: OpenAPIV3.HttpMethods) => {
+  if (httpMethod === 'get' || httpMethod === 'delete') {
+    return false;
+  }
+  return true;
+};
 
 export const getOpenApiPathsObject = (
   appRouter: OpenApiRouter,
   pathsObject: OpenAPIV3.PathsObject,
 ): OpenAPIV3.PathsObject => {
   const { queries, mutations, subscriptions } = appRouter._def;
-
-  forEachOpenApiProcedure(queries, ({ path: queryPath, procedure, openapi }) => {
-    try {
-      const { method, protect, summary, description, tags, tag, headers } = openapi;
-      if (method !== 'GET' && method !== 'DELETE') {
-        throw new TRPCError({
-          message: 'Query method must be GET or DELETE',
-          code: 'INTERNAL_SERVER_ERROR',
-        });
-      }
-
-      const path = normalizePath(openapi.path);
-      const pathParameters = getPathParameters(path);
-      const headerParameters = headers?.map((header) => ({ ...header, in: 'header' })) || [];
-      const httpMethod = OpenAPIV3.HttpMethods[method];
-      if (pathsObject[path]?.[httpMethod]) {
-        throw new TRPCError({
-          message: `Duplicate procedure defined for route ${method} ${path}`,
-          code: 'INTERNAL_SERVER_ERROR',
-        });
-      }
-
-      const { inputParser, outputParser } = getInputOutputParsers(procedure);
-
-      pathsObject[path] = {
-        ...pathsObject[path],
-        [httpMethod]: {
-          operationId: queryPath,
-          summary,
-          description,
-          tags: tags ?? (tag ? [tag] : undefined),
-          security: protect ? [{ Authorization: [] }] : undefined,
-          parameters: [
-            ...headerParameters,
-            ...(getParameterObjects(inputParser, pathParameters, 'all') || []),
-          ],
-          responses: getResponsesObject(outputParser),
-        },
-      };
-    } catch (error: any) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      error.message = `[query.${queryPath}] - ${error.message}`;
-      throw error;
-    }
-  });
-
-  forEachOpenApiProcedure(mutations, ({ path: mutationPath, procedure, openapi }) => {
-    try {
-      const { method, protect, summary, description, tags, tag, headers } = openapi;
-      if (method !== 'POST' && method !== 'PATCH' && method !== 'PUT') {
-        throw new TRPCError({
-          message: 'Mutation method must be POST, PATCH or PUT',
-          code: 'INTERNAL_SERVER_ERROR',
-        });
-      }
-
-      const path = normalizePath(openapi.path);
-      const pathParameters = getPathParameters(path);
-      const headerParameters = headers?.map((header) => ({ ...header, in: 'header' })) || [];
-      const httpMethod = OpenAPIV3.HttpMethods[method];
-      if (pathsObject[path]?.[httpMethod]) {
-        throw new TRPCError({
-          message: `Duplicate procedure defined for route ${method} ${path}`,
-          code: 'INTERNAL_SERVER_ERROR',
-        });
-      }
-
-      const { inputParser, outputParser } = getInputOutputParsers(procedure);
-
-      pathsObject[path] = {
-        ...pathsObject[path],
-        [httpMethod]: {
-          operationId: mutationPath,
-          summary,
-          description,
-          tags: tags ?? (tag ? [tag] : undefined),
-          security: protect ? [{ Authorization: [] }] : undefined,
-          requestBody: getRequestBodyObject(inputParser, pathParameters),
-          parameters: [
-            ...headerParameters,
-            ...(getParameterObjects(inputParser, pathParameters, 'path') || []),
-          ],
-          responses: getResponsesObject(outputParser),
-        },
-      };
-    } catch (error: any) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      error.message = `[mutation.${mutationPath}] - ${error.message}`;
-      throw error;
-    }
-  });
 
   forEachOpenApiProcedure(subscriptions, ({ path: subscriptionPath }) => {
     try {
@@ -112,6 +32,63 @@ export const getOpenApiPathsObject = (
     } catch (error: any) {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       error.message = `[subscription.${subscriptionPath}] - ${error.message}`;
+      throw error;
+    }
+  });
+
+  const procedures = mergeProcedureRecords(queries, mutations);
+
+  forEachOpenApiProcedure(procedures, ({ path: operationId, procedure, openapi }) => {
+    try {
+      const { method, protect, summary, description, tags, tag, headers } = openapi;
+
+      const path = normalizePath(openapi.path);
+      const pathParameters = getPathParameters(path);
+      const headerParameters = headers?.map((header) => ({ ...header, in: 'header' })) || [];
+      const httpMethod = OpenAPIV3.HttpMethods[method];
+      if (!httpMethod) {
+        throw new TRPCError({
+          message: 'Method must be GET, POST, PATCH, PUT or DELETE',
+          code: 'INTERNAL_SERVER_ERROR',
+        });
+      }
+      if (pathsObject[path]?.[httpMethod]) {
+        throw new TRPCError({
+          message: `Duplicate procedure defined for route ${method} ${path}`,
+          code: 'INTERNAL_SERVER_ERROR',
+        });
+      }
+
+      const { inputParser, outputParser } = getInputOutputParsers(procedure);
+
+      pathsObject[path] = {
+        ...pathsObject[path],
+        [httpMethod]: {
+          operationId,
+          summary,
+          description,
+          tags: tags ?? (tag ? [tag] : undefined),
+          security: protect ? [{ Authorization: [] }] : undefined,
+          ...(acceptsRequestBody(httpMethod)
+            ? {
+                requestBody: getRequestBodyObject(inputParser, pathParameters),
+                parameters: [
+                  ...headerParameters,
+                  ...(getParameterObjects(inputParser, pathParameters, 'path') || []),
+                ],
+              }
+            : {
+                parameters: [
+                  ...headerParameters,
+                  ...(getParameterObjects(inputParser, pathParameters, 'all') || []),
+                ],
+              }),
+          responses: getResponsesObject(outputParser),
+        },
+      };
+    } catch (error: any) {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      error.message = `[${operationId}] - ${error.message}`;
       throw error;
     }
   });

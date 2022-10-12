ea68188424a5836dab56c43a722da31558f3bae7
James Berry <jb@jamesbe.com>
2022-10-12 13:56:31 +0100

    tidy pr

--- src/adapters/aws-lambda.ts
+++ src/adapters/aws-lambda.ts
@@ -1,8 +1,7 @@
 import { TRPCError } from '@trpc/server';
 import {
   APIGatewayEvent,
-  APIGatewayResult,
-  CreateAWSLambdaContextOptions,
+  AWSLambdaOptions,
   UNKNOWN_PAYLOAD_FORMAT_VERSION_ERROR_MESSAGE,
   getHTTPMethod,
   getPath,
@@ -13,49 +12,21 @@ import {
 import type { NodeHTTPRequest } from '@trpc/server/dist/adapters/node-http';
 import type { Context as APIGWContext } from 'aws-lambda';
 import { EventEmitter } from 'events';
-import type { IncomingMessage, ServerResponse } from 'http';
 import type { Body, RequestMethod } from 'node-mocks-http';
 import { createRequest, createResponse } from 'node-mocks-http';
 
 import type { OpenApiRouter } from '../types';
-import {
-  CreateOpenApiNodeHttpHandlerOptions,
-  createOpenApiNodeHttpHandler,
-} from './node-http/core';
-
-type AWSLambdaCreateContextFn<TRouter extends OpenApiRouter, TEvent extends APIGatewayEvent> = ({
-  event,
-  context,
-}: CreateAWSLambdaContextOptions<TEvent>) =>
-  | TRouter['_def']['_config']['$types']['ctx']
-  | Promise<TRouter['_def']['_config']['$types']['ctx']>;
+import { createOpenApiNodeHttpHandler } from './node-http/core';
 
 export type CreateOpenApiAwsLambdaHandlerOptions<
   TRouter extends OpenApiRouter,
   TEvent extends APIGatewayEvent,
-> = Omit<
-  CreateOpenApiNodeHttpHandlerOptions<TRouter, IncomingMessage, ServerResponse>,
-  'createContext'
-> &
-  (
-    | {
-        /**
-         * @link https://trpc.io/docs/context
-         **/
-        createContext: AWSLambdaCreateContextFn<TRouter, TEvent>;
-      }
-    | {
-        /**
-         * @link https://trpc.io/docs/context
-         **/
-        createContext?: AWSLambdaCreateContextFn<TRouter, TEvent>;
-      }
-  );
+> = Pick<
+  AWSLambdaOptions<TRouter, TEvent>,
+  'router' | 'createContext' | 'responseMeta' | 'onError'
+>;
 
-/* -------------------------------------------------------------------------- */
-/*                          Mock Request and Response                         */
-/* -------------------------------------------------------------------------- */
-function createRequestFromEvent(event: APIGatewayEvent): NodeHTTPRequest {
+const createMockNodeHTTPRequest = (event: APIGatewayEvent): NodeHTTPRequest => {
   const path = getPath(event);
   const url = event.requestContext.domainName
     ? `https://${event.requestContext.domainName}/${getPath(event)}`
@@ -64,38 +35,20 @@ function createRequestFromEvent(event: APIGatewayEvent): NodeHTTPRequest {
     : `/${path}`;
 
   const method = getHTTPMethod(event).toUpperCase() as RequestMethod;
-  const req = createRequest({
+  const body = typeof event.body === 'string' ? (JSON.parse(event.body) as Body) : undefined;
+
+  return createRequest({
     url,
     method,
     query: event.queryStringParameters || undefined,
-    body: event.body ? (JSON.parse(event.body) as Body) : undefined,
     headers: event.headers,
+    body,
   });
-  return req;
-}
+};
 
-function createLambdaResponse<TEvent extends APIGatewayEvent, TResult extends APIGatewayResult>(
-  event: TEvent,
-  response: ReturnType<typeof createResponse>,
-): TResult {
-  const resp = {
-    statusCode: response.statusCode,
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    body: response._getData() ?? '',
-    headers: transformHeaders(response._getHeaders() || {}),
-  };
-
-  if (isPayloadV1(event)) {
-    return resp as TResult;
-  } else if (isPayloadV2(event)) {
-    return resp as TResult;
-  } else {
-    throw new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: UNKNOWN_PAYLOAD_FORMAT_VERSION_ERROR_MESSAGE,
-    });
-  }
-}
+const createMockNodeHTTPResponse = () => {
+  return createResponse({ eventEmitter: EventEmitter });
+};
 
 export const createOpenApiAwsLambdaHandler = <
   TRouter extends OpenApiRouter,
@@ -104,18 +57,25 @@ export const createOpenApiAwsLambdaHandler = <
   opts: CreateOpenApiAwsLambdaHandlerOptions<TRouter, TEvent>,
 ) => {
   return async (event: TEvent, context: APIGWContext) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const createContext = async () => opts.createContext?.({ event, context });
-    const openAwsLambdaHandler = createOpenApiNodeHttpHandler({
-      ...opts,
-      createContext,
-    });
-    const _req = createRequestFromEvent(event);
-    const _res = createResponse({
-      eventEmitter: EventEmitter,
-    });
+    const openApiHttpHandler = createOpenApiNodeHttpHandler({ ...opts, createContext } as any);
 
-    await openAwsLambdaHandler(_req, _res);
-    return createLambdaResponse(event, _res);
+    const req = createMockNodeHTTPRequest(event);
+    const res = createMockNodeHTTPResponse();
+
+    await openApiHttpHandler(req, res);
+
+    if (isPayloadV1(event) || isPayloadV2(event)) {
+      return {
+        statusCode: res.statusCode,
+        headers: transformHeaders(res._getHeaders() || {}),
+        body: res._getData(),
+      };
+    } else {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: UNKNOWN_PAYLOAD_FORMAT_VERSION_ERROR_MESSAGE,
+      });
+    }
   };
 };

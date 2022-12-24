af079252669a4c495329378ae5f462b95a8e4176
Anthony Shew <anthonyshew@gmail.com>
2022-12-24 10:48:53 -0700

    Typo in with-nextjs example
    Just a missing letter. :)

--- examples/with-nextjs/src/pages/api/openapi.json.ts
+++ examples/with-nextjs/src/pages/api/openapi.json.ts
@@ -3,8 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { openApiDocument } from '../../server/openapi';
 
 // Respond with our OpenAPI schema
-const hander = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).send(openApiDocument);
 };
 
-export default hander;
+export default handler;
